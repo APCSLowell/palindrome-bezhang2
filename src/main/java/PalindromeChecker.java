40301a9db8852371ff4a1c5f258dec8f134da61e
bezhang2 <143027961+bezhang2@users.noreply.github.com>
2024-02-02 22:20:38 -0800

    Update PalindromeChecker.java

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -37,40 +37,13 @@ public void tester()
 }
 public boolean palindrome(String word)
 {
-  String willy = noCapitals(noSpaces(onlyLetters(word)));
-  if (willy.equals(reverse(willy))) {
-    return true;
-  }
+  //your code here
   return false;
 }
 public String reverse(String str)
 {
-  String sNew = new String();
-  for (int i = str.length(); i > 0; i--) {
-    sNew = sNew + str.substring(i - 1, i);
-  }
-  return sNew;
-}
-public String noCapitals(String sWord) {
-  return sWord.toLowerCase();
-}
-public String noSpaces(String sWord) {
-  String willy = "";
-  for (int i = 0; i < sWord.length(); i++) {
-    if (!sWord.substring(i, i + 1).equals(" ")) {
-      willy = willy + sWord.substring(i, i + 1);
-    }
-  }
-  return willy;
-}
-public String onlyLetters(String sString) {
-  String willy = "";
-  for (int i = 0; i < sString.length(); i++) {
-    if (Character.isLetter(sString.charAt(i))) {
-      willy = willy + sString.charAt(i);
-    }
-  }
-  return willy;
+    String sNew = new String();
+    //your code here
+    return sNew;
 }
 }
-
